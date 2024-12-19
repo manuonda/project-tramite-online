4ac2e967e27612279dd12abbd7f9b09672b39eb6
david garcia <david.garcia@grupomost.com>
2024-12-19 16:20:06 -0300

    Refactor SectionRepositoryTest: update test annotations and add application-test.properties

--- back/src/test/java/com/tramite/online/repository/SectionRepositoryTest.java
+++ back/src/test/java/com/tramite/online/repository/SectionRepositoryTest.java
@@ -1,34 +1,11 @@
 package com.tramite.online.repository;
 
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.Test;
 
-import com.tramite.online.domain.entity.Section;
-
-@DataJpaTest
-public class SectionRepositoryTest {
-
-    @Autowired
-    private SectionRepository sectionRepository;
-
-    private Section section;
-    @BeforeEach
-    public void init(){
-
-    }
-
+import static org.junit.jupiter.api.Assertions.*;
+class SectionRepositoryTest {
 
     @Test
-    @DisplayName("Add Section")
-    public void givenSection_whenSaveEntity_thenReturnObjectSection(){
-        //given
-        Section section = new Section();
-        
-        //when
-        //then
+    void findByName() {
     }
-
 }
