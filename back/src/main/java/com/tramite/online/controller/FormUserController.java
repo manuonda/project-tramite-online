fe72841df57d00418b080785b8bb21c6874ca44a
david garcia <david.garcia@grupomost.com>
2025-02-18 23:23:38 -0300

    Enhance FormUserController with Swagger documentation for GET and POST endpoints; add validation for createFormUser method

--- back/src/main/java/com/tramite/online/controller/FormUserController.java
+++ back/src/main/java/com/tramite/online/controller/FormUserController.java
@@ -7,16 +7,21 @@ import com.tramite.online.domain.models.FormUserDTO;
 import com.tramite.online.domain.models.PagedResult;
 import com.tramite.online.service.FormUserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 import static com.tramite.online.constants.GeneralConstants.ASC;
 import static com.tramite.online.constants.GeneralConstants.DEFAULT_PAGE_NUMBER;
 import static com.tramite.online.constants.GeneralConstants.DEFAULT_PAGE_SIZE;
 
+import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties.Http;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +29,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
+//https://blog.jetbrains.com/idea/2025/02/database-migrations-in-the-real-world/
 
 @RestController
 @RequestMapping("/api/v1/forms")
@@ -41,9 +47,15 @@ public class FormUserController {
         this.formUserService = formUserService;
     }
 
-
-
     @GetMapping
+    @Operation(
+        summary = "Get all forms",
+        description = "Get all forms with pagination"
+    )
+    @ApiResponse(
+        responseCode ="200",
+        description = "Return all forms"
+    )
     public ResponseEntity<PagedResult<FormUserDTO>> getAll(
       @RequestParam(defaultValue = DEFAULT_PAGE_NUMBER) int page,
       @RequestParam(defaultValue = DEFAULT_PAGE_SIZE) int size,
@@ -55,14 +67,23 @@ public class FormUserController {
     }
 
 
-    @PostMapping("path")
-    public String postMethodName(@RequestBody String entity) {
-        //TODO: process POST request
-        
-        return entity;
+    @PostMapping
+    @Operation(
+        summary ="Create a new form user",
+        description = "Create a new form user"
+    )
+    @ApiResponse(
+        responseCode = "201",
+        description = "Create a new form user"
+    )
+    public ResponseEntity<FormUserDTO>  createFormUser(@Valid @RequestBody FormUserDTO formUserDTO){ {
+        log.info("Create form user dto {}", formUserDTO);
+        FormUserDTO saveUserDTO = this.formUserService.save(formUserDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saveUserDTO);
     }
 
-    @PutMapping(ID_IN_PATH)
     
+
+   
     
 }
