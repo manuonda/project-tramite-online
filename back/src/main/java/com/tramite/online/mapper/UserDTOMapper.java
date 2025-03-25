a43b0b6ced9fee9441062867bed9b0c195ea3e86
david garcia <david.garcia@grupomost.com>
2025-03-25 10:13:47 -0300

    Add UserDTOMapper class; implement Function interface for mapping User to UserDTO

---
+++ back/src/main/java/com/tramite/online/mapper/UserDTOMapper.java
@@ -0,0 +1,18 @@
+package com.tramite.online.mapper;
+
+import java.util.function.Function;
+
+import com.tramite.online.domain.entity.User;
+import com.tramite.online.domain.models.UserDTO;
+
+//link : https://www.youtube.com/watch?v=5yquJa2x3Ko
+
+public class UserDTOMapper implements Function<User, UserDTO>{
+
+    @Override
+    public UserDTO apply(User t) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'apply'");
+    }
+
+}
