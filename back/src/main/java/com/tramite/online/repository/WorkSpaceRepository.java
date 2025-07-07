d6a62b5d8b16a4f23f0c5e01fded713255e9eb77
david garcia <david.garcia@grupomost.com>
2025-07-07 09:12:23 -0300

    feat: create WorkSpace repository and service classes

---
+++ back/src/main/java/com/tramite/online/repository/WorkSpaceRepository.java
@@ -0,0 +1,9 @@
+package com.tramite.online.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tramite.online.domain.entity.WorkSpace;
+
+public interface WorkSpaceRepository extends JpaRepository<WorkSpace, Long>{
+
+}
---
+++ back/src/main/java/com/tramite/online/service/WorkSpaceService.java
@@ -0,0 +1,8 @@
+package com.tramite.online.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WorkSpaceService {
+
+}
